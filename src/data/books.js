b37a55b7ca06d347d211c2e1d0233a92b743b88d
joseipiazza94 <joseipiazza94@gmail.com>
2026-02-09 17:28:23 -0300

    Sección Libros Terminada: Carrusel 3D Infinito, resposive, y galería arreglada

--- src/data/books.js
+++ src/data/books.js
@@ -1,118 +1,61 @@
+// Libro 1: Hairy Worm and his Lost Rythm
 import coverHairyWorm from '../assets/books/hairy-worm-book/hairy-worm-cover.png';
-import page01HairyWorm from '../assets/books/hairy-worm-book/hairy-worm-page-01.png';
-import page02HairyWorm from '../assets/books/hairy-worm-book/hairy-worm-page-14.png';
-import doublePage01HairyWorm from '../assets/books/hairy-worm-book/hairy-worm-pages-09-10.png';
+import page1HairyWorm from '../assets/books/hairy-worm-book/hairy-worm-page-01.png';
+import page2HairyWorm from '../assets/books/hairy-worm-book/hairy-worm-page-14.png';
+import page3HairyWorm from '../assets/books/hairy-worm-book/hairy-worm-pages-09-10.png';
+
+// Libro 2: A Chipito in Paris
+import coverChipitoParis from '../assets/books/chipito-paris-book/chipito-paris-cover.png';
+import page1ChipitoParis from '../assets/books/chipito-paris-book/chipito-paris-page-01.png';
+import page2ChipitoParis from '../assets/books/chipito-paris-book/chipito-paris-page-03.png';
+import page3ChipitoParis from '../assets/books/chipito-paris-book/chipito-paris-page-05.png';
+
+// Libro 3: Ashton no es solo una Tortuga
+import coverAshtonTortuga from '../assets/books/ashton-tortuga-book/ashton-tortuga-cover.jpg';
+import page1AshtonTortuga from '../assets/books/ashton-tortuga-book/ashton-tortuga-page-02.jpg';
+import page2AshtonTortuga from '../assets/books/ashton-tortuga-book/ashton-tortuga-page-03.jpg';
+import page3AshtonTortuga from '../assets/books/ashton-tortuga-book/ashton-tortuga-page-04.jpg';
 
 export const books = [
     {
-        id: "hairy-worm-book",
-        titulo: "A Very Hairy Worm & his Lost Rythm",
-        autor: "Emily Dungan",
-        year: "2024",
+        id: 1,
+        title: "A very Hairy Worm and his Lost Rythm",
 
-        portada: coverHairyWorm,
+        cover: coverHairyWorm,
 
-        paginas: [
-            {
-                img: coverHairyWorm,
-                desc: "Portada"
-            },
-            {
-                img: page01HairyWorm,
-                desc: "Página 1 - Introducción"
-            },
-            {
-                img: page02HairyWorm,
-                desc: "Página 14 - El grillo Millard"
-            },
-            {
-                img: doublePage01HairyWorm,
-                desc: "Páginas 9 y 10 - Bella, la mariposa"
-            }
+        gallery: [
+            {src: coverHairyWorm, alt: "Portada del libro" },
+            {src: page1HairyWorm, alt: "Pagina 1 - Presentacion"},
+            {src: page2HairyWorm, alt: "Pagina 14 - El Grillo Millard"},
+            {src: page3HairyWorm, alt: "Paginas 9 y 10 - Bella, la Mariposa"}
         ]
     },
 
     {
-        id: "hairy-worm-book",
-        titulo: "A Very Hairy Worm & his Lost Rythm",
-        autor: "Emily Dungan",
-        year: "2024",
+        id: 2,
+        title: "A Chipito in Paris",
 
-        portada: coverHairyWorm,
+        cover: coverChipitoParis,
 
-        paginas: [
-            {
-                img: coverHairyWorm,
-                desc: "Portada"
-            },
-            {
-                img: page01HairyWorm,
-                desc: "Página 1 - Introducción"
-            },
-            {
-                img: page02HairyWorm,
-                desc: "Página 14 - El grillo Millard"
-            },
-            {
-                img: doublePage01HairyWorm,
-                desc: "Páginas 9 y 10 - Bella, la mariposa"
-            }
+        gallery: [
+            {src: coverChipitoParis, alt: "Portada del libro" },
+            {src: page1ChipitoParis, alt: "Pagina 1 - Presentacion"},
+            {src: page2ChipitoParis, alt: "Pagina 14 - El Grillo Millard"},
+            {src: page3ChipitoParis, alt: "Paginas 9 y 10 - Bella, la Mariposa"}
         ]
     },
 
     {
-        id: "hairy-worm-book",
-        titulo: "A Very Hairy Worm & his Lost Rythm",
-        autor: "Emily Dungan",
-        year: "2024",
+        id: 3,
+        title: "Ashton no es solo una Tortuga",
 
-        portada: coverHairyWorm,
+        cover: coverAshtonTortuga,
 
-        paginas: [
-            {
-                img: coverHairyWorm,
-                desc: "Portada"
-            },
-            {
-                img: page01HairyWorm,
-                desc: "Página 1 - Introducción"
-            },
-            {
-                img: page02HairyWorm,
-                desc: "Página 14 - El grillo Millard"
-            },
-            {
-                img: doublePage01HairyWorm,
-                desc: "Páginas 9 y 10 - Bella, la mariposa"
-            }
+        gallery: [
+            {src: coverAshtonTortuga, alt: "Portada del libro" },
+            {src: page1AshtonTortuga, alt: "Pagina 2 - Casamiento"},
+            {src: page2AshtonTortuga, alt: "Pagina 14 - Flamingo Gardens"},
+            {src: page3AshtonTortuga, alt: "Paginas 9 y 10 - Pool Time"}
         ]
-    },
-
-    {
-        id: "hairy-worm-book",
-        titulo: "A Very Hairy Worm & his Lost Rythm",
-        autor: "Emily Dungan",
-        year: "2024",
-
-        portada: coverHairyWorm,
-
-        paginas: [
-            {
-                img: coverHairyWorm,
-                desc: "Portada"
-            },
-            {
-                img: page01HairyWorm,
-                desc: "Página 1 - Introducción"
-            },
-            {
-                img: page02HairyWorm,
-                desc: "Página 14 - El grillo Millard"
-            },
-            {
-                img: doublePage01HairyWorm,
-                desc: "Páginas 9 y 10 - Bella, la mariposa"
-            }
-        ]
-    },
-];
+    }
+]
