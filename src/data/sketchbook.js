df7142762b8e8378c6f93550823a3a5d40115236
joseipiazza94 <joseipiazza94@gmail.com>
2026-02-12 17:24:55 -0300

    Formulario de contacto funcional y limpieza de footer duplicado

---
+++ src/data/sketchbook.js
@@ -0,0 +1,51 @@
+import bambiSkecth from '../assets/sketchbook/bambi-sketch.png';
+import florSketch from '../assets/sketchbook/flor-sketch.png';
+import loboSketch from '../assets/sketchbook/lobo-nube-sketch.png';
+import loguitoSketch from '../assets/sketchbook/loguito-sketch.png';
+import osoSketch from '../assets/sketchbook/oso-tigre-sketch.png';
+import piedraSketch from '../assets/sketchbook/piedritas-sketch.png';
+
+
+export const sketches = [
+    {
+        id: "bambi-sketch",
+        imagen: bambiSkecth,
+        alt: "Bambi",
+        desc:"Dibujo de un bambi"
+    },
+
+    {
+        id: "flor-sketch",
+        imagen: florSketch,
+        alt: "Flor",
+        desc:"Dibujo de una flor"
+    },
+
+    {
+        id: "lobo-nube-sketch",
+        imagen: loboSketch,
+        alt: "Lobo Nube",
+        desc:"Dibujo de un lobo en una nube"
+    },
+
+    {
+        id: "nuevo-logo-sketch",
+        imagen: loguitoSketch,
+        alt: "Nuevo Logo",
+        desc:"Dibujo de mi logo"
+    },
+
+    {
+        id: "oso-tigre-sketch",
+        imagen: osoSketch,
+        alt: "Un oso y un tigre",
+        desc:"Dibujo de un oso y un tigre"
+    },
+
+    {
+        id: "piedritas-sketch",
+        imagen: piedraSketch,
+        alt: "Piedritas",
+        desc:"Dibujo de piedritas"
+    }
+]
