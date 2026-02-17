76ba8a655fb20428dea7ce562114418d5232e282
joseipiazza94 <joseipiazza94@gmail.com>
2026-02-17 17:42:12 -0300

    Implementación completa de i18n (Inglés/Español) y mejoras de diseño

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,4 +4,11 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://portfolio-flor.vercel.app',
+    i18n: {
+        defaultLocale: "es",
+        locales: ["es", "en"],
+        routing: {
+            prefixDefaultLocale: false
+        }
+    }
 });
