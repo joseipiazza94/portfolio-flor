2549e1d3c5a98dcc87054b21ce4723f427a98998
joseipiazza94 <joseipiazza94@gmail.com>
2026-02-23 15:05:45 -0300

    Test de libro: Esperando contenido definitivo

--- src/data/books.js
+++ src/data/books.js
@@ -16,6 +16,18 @@ import page1AshtonTortuga from '../assets/books/ashton-tortuga-book/ashton-tortu
 import page2AshtonTortuga from '../assets/books/ashton-tortuga-book/ashton-tortuga-page-03.jpg';
 import page3AshtonTortuga from '../assets/books/ashton-tortuga-book/ashton-tortuga-page-04.jpg';
 
+// Libro 4: Las Aventuras de Caracacha
+import caracachaCover from '../assets/books/caracacha-aventuras-book/caracacha-single-cover.jpg';
+import caracachaDoubleCover from '../assets/books/caracacha-aventuras-book/caracacha-cover.jpg';
+import caracachaChars1 from '../assets/books/caracacha-aventuras-book/caracacha-char-1.jpg';
+import caracachaChars2 from '../assets/books/caracacha-aventuras-book/caracacha-char-2.jpg';
+import caracachaLogo from '../assets/books/caracacha-aventuras-book/caracacha-double-logo.jpg';
+import caracachaDoublePage from '../assets/books/caracacha-aventuras-book/caracacha-double-illustration.jpg';
+import caracachaSketch from '../assets/books/caracacha-aventuras-book/caracacha-sketch.jpg';
+import caracachaDetails from '../assets/books/caracacha-aventuras-book/caracacha-endpaper-details.jpg';
+import caracachaMockup from '../assets/books/caracacha-aventuras-book/caracacha-book-cover-ty.jpg';
+
+
 export const books = [
     {
         id: 1,
@@ -56,6 +68,24 @@ export const books = [
             {src: page1HairyWorm, alt: { es: "Portada del libro", en: "Book cover"}},
             {src: page2HairyWorm, alt: { es: "Portada del libro", en: "Book cover"}},
             {src: page3HairyWorm, alt: { es: "Portada del libro", en: "Book cover"}},
+        ]
+    },
+
+    {
+        id: 4,
+        title: "Las Aventuras de Caracacha",
+
+        cover: caracachaCover,
+
+        gallery: [
+            {src: caracachaDoubleCover, alt: { es: "Portada del libro", en: "Book cover"}},
+            {src: caracachaChars1, alt: { es: "Personajes Principales", en: "Main Characters"}},
+            {src: caracachaChars2, alt: { es: "Personajes Secundarios", en: "Secondary Characters"}},
+            {src: caracachaLogo, alt: { es: "Mockup - Pagina Doble", en: "Double Page Mockup"}},
+            {src: caracachaDoublePage, alt: { es: "Ilustraciones", en: "Illustrations"}},
+            {src: caracachaSketch, alt: { es: "Sketches", en: "Sketches"}},
+            {src: caracachaDetails, alt: { es: "Detalles y Guardas", en: "Endpapers and Details"}},
+            {src: caracachaMockup, alt: { es: "Mockup", en: "Mockup"}},
         ]
     }
 ]
