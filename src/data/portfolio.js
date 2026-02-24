3dee03fae0a15bf41bc31b2f70f69097021cf2f1
joseipiazza94 <joseipiazza94@gmail.com>
2026-02-24 17:46:43 -0300

    Imagenes agregadas al Portfolio

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -1,33 +1,222 @@
-import imgCluckCooChicken from '../assets/portfolio/cluck-cluck-coo-chicken-libro-ilustrado.png';
-import imgAshtonTortugaFlamengo from '../assets/portfolio/ashton-tortuga-flamengo-libro-ilustrado.jpg';
-import imgFishyOnFire from '../assets/portfolio/fishy-adventure-fire-libro-ilustrado.png';
-import imgHatNestEggs from '../assets/portfolio/hat-nest-eggs-libro-ilustrado.jpg';
-import imgColorfulCaterpillar from '../assets/portfolio/colorfull-caterpillar-libro-ilustrado.png';
-import imgCaracachaEzio from '../assets/portfolio/peraclus-ezio-caracacha.png';
-import imgBrujaCaracacha from '../assets/portfolio/caracacha-full.png';
-import imgChipitoInParis from '../assets/portfolio/chipito.png';
+import img01 from "../assets/portfolio/caracacha-full.png";
+import img02 from "../assets/portfolio/cluck-cluck-coo-chicken-libro-ilustrado.png";
+import img03 from "../assets/portfolio/the-journey-of-friendship-berni.png";
+import img04 from "../assets/portfolio/the-glowshire-festival.png";
+import img05 from "../assets/portfolio/the-secret-superpower.png";
+import img06 from "../assets/portfolio/jammie-ferocious-jaguar.png";
+import img07 from "../assets/portfolio/colorfull-caterpillar-libro-ilustrado.png";
+import img08 from "../assets/portfolio/school-kids.png";
+import img09 from "../assets/portfolio/sisterhood.png";
+import img10 from "../assets/portfolio/Selfportrait.png";
 
 export const ilustraciones = [
-    {
-        id: "cluck-coo-chicken-1",
-        imagen: imgCluckCooChicken,
-        alt: {
-            es:"Niño nadando con sus animales",
-            en: "Kid swimming with his animals"
-            },
-        meta: {
-            "title": "Cluck, Cluck, Coo Chicken",
-            "year": "2022",
-            "technique": {
-                es: "Ilustración digital",
-                en: "Digital illustration"
-            },
-            "client": {
-                es: "Carlitos",
-                en: "Charles"
-            }
-        }
+  {
+    id: "img01",
+    imagen: img01,
+    alt: {
+      es: "La Brujita Caracacha en Sipa Sipa",
+      en: "Little Witch Caracacha in Sipa Sipa",
     },
+    meta: {
+      title: "Caracacha en Sipa Sipa - Las Aventuras de Caracacha",
+      year: "2025",
+      technique: {
+        es: "Ilustración digital",
+        en: "Digital illustration",
+      },
+      client: {
+        es: "para Susana Maggio",
+        en: "for Susana Maggio",
+      },
+    },
+  },
 
-   
-]
+  {
+    id: "img02",
+    imagen: img02,
+    alt: {
+      es: "Niño nadando con sus animalitos de la granja",
+      en: "Boy swimming with his farm animals",
+    },
+    meta: {
+      title: "Cluck, Cluck, Coo Chicken",
+      year: "2022",
+      technique: {
+        es: "Ilustración digital",
+        en: "Digital illustration",
+      },
+      client: {
+        es: "Comisión Privada",
+        en: "Private Commission",
+      },
+    },
+  },
+
+  {
+    id: "img03",
+    imagen: img03,
+    alt: {
+      es: "Dos amigas navegando en un barquito de papel",
+      en: "Two friends sailing on a paper boat",
+    },
+    meta: {
+      title: "The Journey of Friendship",
+      year: "2022",
+      technique: {
+        es: "Ilustración digital",
+        en: "Digital illustration",
+      },
+      client: {
+        es: "Comisión Privada",
+        en: "Private Commission",
+      },
+    },
+  },
+
+  {
+    id: "img04",
+    imagen: img04,
+    alt: {
+      es: "Dos amigas navegando en un barquito de papel",
+      en: "Two friends sailing on a paper boat",
+    },
+    meta: {
+      title: "The Glowshire Festival - Flicker",
+      year: "2024",
+      technique: {
+        es: "Ilustración digital",
+        en: "Digital illustration",
+      },
+      client: {
+        es: "para Alyssa Christine",
+        en: "for Alyssa Christine",
+      },
+    },
+  },
+
+  {
+    id: "img05",
+    imagen: img05,
+    alt: {
+      es: "Dos amigas navegando en un barquito de papel",
+      en: "Two friends sailing on a paper boat",
+    },
+    meta: {
+      title: "The Glowshire Festival - Flicker",
+      year: "2024",
+      technique: {
+        es: "Ilustración digital",
+        en: "Digital illustration",
+      },
+      client: {
+        es: "para Alyssa Christine",
+        en: "for Alyssa Christine",
+      },
+    },
+  },
+
+  {
+    id: "img06",
+    imagen: img06,
+    alt: {
+      es: "Dos amigas navegando en un barquito de papel",
+      en: "Two friends sailing on a paper boat",
+    },
+    meta: {
+      title: "The Glowshire Festival - Flicker",
+      year: "2024",
+      technique: {
+        es: "Ilustración digital",
+        en: "Digital illustration",
+      },
+      client: {
+        es: "para Alyssa Christine",
+        en: "for Alyssa Christine",
+      },
+    },
+  },
+
+  {
+    id: "img07",
+    imagen: img07,
+    alt: {
+      es: "Dos amigas navegando en un barquito de papel",
+      en: "Two friends sailing on a paper boat",
+    },
+    meta: {
+      title: "The Glowshire Festival - Flicker",
+      year: "2024",
+      technique: {
+        es: "Ilustración digital",
+        en: "Digital illustration",
+      },
+      client: {
+        es: "para Alyssa Christine",
+        en: "for Alyssa Christine",
+      },
+    },
+  },
+
+  {
+    id: "img08",
+    imagen: img08,
+    alt: {
+      es: "Dos amigas navegando en un barquito de papel",
+      en: "Two friends sailing on a paper boat",
+    },
+    meta: {
+      title: "The Glowshire Festival - Flicker",
+      year: "2024",
+      technique: {
+        es: "Ilustración digital",
+        en: "Digital illustration",
+      },
+      client: {
+        es: "para Alyssa Christine",
+        en: "for Alyssa Christine",
+      },
+    },
+  },
+
+  {
+    id: "img09",
+    imagen: img09,
+    alt: {
+      es: "Dos amigas navegando en un barquito de papel",
+      en: "Two friends sailing on a paper boat",
+    },
+    meta: {
+      title: "The Glowshire Festival - Flicker",
+      year: "2024",
+      technique: {
+        es: "Ilustración digital",
+        en: "Digital illustration",
+      },
+      client: {
+        es: "para Alyssa Christine",
+        en: "for Alyssa Christine",
+      },
+    },
+  },
+
+  {
+    id: "img10",
+    imagen: img10,
+    alt: {
+      es: "Yo, Flor!",
+      en: "This is me, FLor!",
+    },
+    meta: {
+      title: "Selfportrait",
+      year: "2024",
+      technique: {
+        es: "Ilustración digital",
+        en: "Digital illustration",
+      },
+      client: {
+        es: "para mi!",
+        en: "for me!",
+      },
+    },
+  },
+];
